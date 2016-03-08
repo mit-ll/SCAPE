8542588b7fc9b731e8cda40eaa21e248e4351b2e
David O'Gwynn <dogwynn@acm.org>
2016-03-08 13:42:10 -0600

    Changed copyright from string to comment, minor fixes for 2/3 compatibility

--- scape/utils/sge.py
+++ scape/utils/sge.py
@@ -1,23 +1,20 @@
-"""
-Copyright (2016) Massachusetts Institute of Technology.  Reproduction/Use 
-of all or any part of this material shall acknowledge the MIT Lincoln 
-Laboratory as the source under the sponsorship of the US Air Force 
-Contract No. FA8721-05-C-0002.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-"""
 #!/usr/bin/python
+# Copyright (2016) Massachusetts Institute of Technology.  Reproduction/Use 
+# of all or any part of this material shall acknowledge the MIT Lincoln 
+# Laboratory as the source under the sponsorship of the US Air Force 
+# Contract No. FA8721-05-C-0002.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 #
 # sge.py
@@ -37,6 +34,7 @@ limitations under the License.
 #
 
 """Submits jobs to Grid Engine.
+
 Handles job dependencies, output redirection, and job script creation.
 """
 
@@ -44,7 +42,7 @@ from __future__ import absolute_import
 import os
 import shutil
 import time
-import StringIO
+from io import StringIO
 import logging
 import xml.etree.cElementTree as cet
 
@@ -192,7 +190,7 @@ def build_job_scripts( directory, jlist ):
             scriptFile.write( 'date 1>&2\n' )
             scriptFile.write( 'echo {} 1>&2\n'.format('END'.center(80,'-')) )
 
-        os.chmod( scriptPath, 0755 )
+        os.chmod( scriptPath, 0o755 )
         job.scriptPath = scriptPath
 
 def extract_submittable_jobs( waiting ):
