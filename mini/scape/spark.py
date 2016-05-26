5381916fac7d81738fe67d51a17f493b8496d995
Ethan Aubin <ethan.aubin@ll.mit.edu>
2016-05-26 08:47:04 -0400

    Update Spark dataframe

--- mini/scape/spark.py
+++ mini/scape/spark.py
@@ -1,48 +1,73 @@
 from __future__ import absolute_import
 
 import pyspark
-from types import MethodType
+from functools import reduce
 
 from scape.registry import DataSource
+import scape.registry as reg
 
-def datasource(readerf, metadata):
-    """Create a data source from a Spark DataFrame or a function returning a DataFrame"""
-    md = scape.functions._create_table_field_tagsdim_map(metadata)
-    if hasattr(readerf, '__call__'):
-        return _SparkDataFrameDataSource(readerf, md)
-    elif isinstance(readerf, pyspark.sql.dataframe.DataFrame):
-        return _SparkDataFrameDataSource(lambda: readerf, md)
+def datasource(dataframe, metadata, description=""):
+    '''Create a data source from a Spark DataFrame or a function returning a DataFrame
+
+    Args:
+      reader: A dataframe of a function returning a dataframe
+      metadata: Mapping from field names to tags and dimensions
+      description: An optional description
+
+    Returns:
+      The Scape datasource wrapping the dataframe supporting equality and regex conditions
+      using '==' and '=~'
+    '''
+    md = reg._create_table_field_tagsdim_map(metadata)
+    if hasattr(dataframe, '__call__'):
+        return _SparkDataFrameDataSource(dataframe, md, description)
+    elif isinstance(dataframe, pyspark.sql.dataframe.DataFrame):
+        return _SparkDataFrameDataSource(lambda: dataframe, md, description)
+
+_dataframe_op_dict = {
+    '==': reg.Equals,
+    '=~': reg.MatchesCond,
+}
+
+def _to_spark_condition(df, cond):
+    if isinstance(cond, reg.Equals):
+        return (df[cond.lhs.name] == cond.rhs)
+    if isinstance(cond, reg.MatchesCond):
+        return df[cond.lhs.name].rlike(cond.rhs)
+    if isinstance(cond, reg.Or):
+        parts = map(lambda c: _to_spark_condition(df, c), cond.parts)
+        return reduce(lambda x,y: (x | y), parts)
+    if isinstance(cond, reg.And):
+        parts = map(lambda c: _to_spark_condition(df, c), cond.parts)
+        return reduce(lambda x, y: (x & y), parts)
 
 class _SparkDataFrameDataSource(DataSource):
-    def __init__(self, readerf, metadata):
+    def __init__(self, readerf, metadata, description):
+        super(_SparkDataFrameDataSource, self).__init__(metadata, description, _dataframe_op_dict)
         self._readerf = readerf
+        self._df = None
         self._metadata = metadata
-    
+
     def connect(self):
-        newdf = self._readerf()
-        setattr(newdf, '__scape_metadata', self._metadata)
-        return newdf
+        if (self._df):
+            return self._df
+        self._df = self._readerf()
+        return self._df
 
-def __or_filtered(df, dsmd, td, value):
-    if isinstance(td, str):
-        td = tagsdim(td)
-    fields = dsmd.fields_matching(td)
-    if not fields:
-        print("Useless filter: Could not find fields matching: " + str(td) + " among\n" + str(dsmd))
-        return df
-    f,rst = fields[0],fields[1:]
-    filterv = df[f]==value
-    for f in rst:
-        filterv = filterv | (df[f]==value)
-    return df.filter(filterv)
+    def select_fields(self, df, select):
+        if select.fields:
+            return df.select(self.field_names(select))
+        else:
+            return df
 
-def __scape_add_registry(self, reg):    
-    self.__scape_metadata = reg
-
-def __scape_or_filter(self, td, value):
-    newdf = __or_filtered(self, self.__scape_metadata, td, value)
-    newdf.__scape_metadata = self.__scape_metadata
-    return newdf
-    
-pyspark.sql.dataframe.DataFrame.add_registry = __scape_add_registry
-pyspark.sql.dataframe.DataFrame.or_filter = __scape_or_filter
+    def run(self, select):
+        ''' Return a dataframe with the given selection.
+        '''
+        cond = self._rewrite(select.condition)
+        df = self.connect()
+        if isinstance(cond, reg.TrueCondition):
+            return self.select_fields(df, select)
+        spark_cond = _to_spark_condition(df, cond)
+        filtered = df.filter(spark_cond)
+        res = self.select_fields(filtered, select)
+        return res
